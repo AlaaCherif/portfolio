19b6bd4d83a84565304a233f24239e48cdfabed6
Alaa <alaac617@gmail.com>
2023-04-13 00:03:41 +0100

    slider finished

--- src/components/AnimatedHero.tsx
+++ src/components/AnimatedHero.tsx
@@ -1,11 +1,39 @@
+import { useState } from 'react';
+
+const words = ['code 🚀', 'value ➕', 'growth 📈'];
+
 export default function AnimatedHero() {
+  const [word, setWord] = useState<string>(words[0]);
+  const nextWord = () => {
+    setWord(prev => {
+      const currentIndex = words.indexOf(prev);
+      return words[currentIndex + 1] || words[0];
+    });
+  };
+  //code value
   return (
     <div className='text-9xl pt-20 leading-relaxed'>
       <div>Striving to make your</div>
       <div>
-        <span>ideas </span>
-        <span className='font-normal'> &#x2192; </span>
-        <span className='bg-white text-black p-2'>code</span>
+        <span>ideas into</span>
+        <h1 className='mb-2 text-9xl md:text-9xl'>
+          <span className='relative'>
+            <span className='h-20 pt-2 overflow-x-hidden whitespace-nowrap text-brand-accent'>
+              {word}
+            </span>
+            <span
+              onAnimationEnd={() => {
+                const element = document.querySelector('#toAnimate');
+                element?.classList.remove('animate-type');
+                void (element as any)?.offsetWidth;
+                nextWord();
+                element?.classList.add('animate-type');
+              }}
+              id='toAnimate'
+              className='cursor absolute -bottom-0 left-0 -top-1 inline-block bg-black w-full will-change animate-type'
+            ></span>
+          </span>
+        </h1>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ export default {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        type: 'type 2.5s ease-out alternate both 2',
+      },
+      keyframes: {
+        type: {
+          '0%': { transform: 'translateX(0ch)' },
+          '10%,20%': { transform: 'translateX(1ch)' },
+          '20%,30%': { transform: 'translateX(2ch)' },
+          '30%,40%': { transform: 'translateX(3ch)' },
+          '40%,50%': { transform: 'translateX(4ch)' },
+          '50%,60%': { transform: 'translateX(5ch)' },
+          '60%,70%': { transform: 'translateX(6ch)' },
+          '70%,80%': { transform: 'translateX(7ch)' },
+          '80%,90%': { transform: 'translateX(8ch)' },
+          '90%,100%': { transform: 'translateX(9ch)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
